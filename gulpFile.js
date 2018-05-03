05a8987f06fe5353e4145e413147cce54201eb3a
Attila Molnar <ate.molnar2@gmail.com>
2018-05-03 18:49:28 +0200

    Add: dist build with gulp and rollup

---
+++ gulpFile.js
@@ -0,0 +1,69 @@
+const gulp = require('gulp');
+const rollup = require('rollup');
+// rollup plugins
+const babel = require("rollup-plugin-babel");
+const rootImport = require("rollup-plugin-root-import");
+const eslint = require("rollup-plugin-eslint");
+const resolve = require("rollup-plugin-node-resolve");
+const uglify = require('rollup-plugin-uglify');
+
+gulp.task('demo-build', function demoBuildTask() {
+	return rollup.rollup({
+    input: "src/range.js",
+  plugins: [
+    eslint({
+      exclude: [
+        "src/styles/**"
+      ]}),
+    babel({ babelrc: false,
+      presets: ["es2015-rollup"],
+      exclude: "node_modules/**"
+    }),
+    rootImport({
+      root: `${__dirname}/src`,
+      useEntry: "prepend",
+      extensions: ".js"
+    }),
+    resolve({
+      browser: true
+    })]
+  })
+    .then((bundle) => {
+      return bundle.write({
+      name: "app",
+      file: "./demo/range.js",
+      format: "iife"
+      });
+    });
+});
+
+gulp.task('dist-build', function demoBuildTask() {
+	return rollup.rollup({
+    input: "src/range.js",
+    plugins: [
+      uglify(),
+    eslint({
+      exclude: [
+        "src/styles/**"
+      ]}),
+    babel({ babelrc: false,
+      presets: ["es2015-rollup"],
+      exclude: "node_modules/**"
+    }),
+    rootImport({
+      root: `${__dirname}/src`,
+      useEntry: "prepend",
+      extensions: ".js"
+    }),
+    resolve({
+      browser: true
+    })]
+  })
+    .then((bundle) => {
+      return bundle.write({
+        name: "app",
+        file: "./dist/range.min.js",
+        format: "iife"
+      });
+    });
+});
